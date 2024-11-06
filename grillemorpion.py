73c1bc6d13f6090ed905e68a085af0635614c404
Manuel-Garcin <manuel.garcin@laplateforme.io>
2024-11-06 10:07:46 +0100

    j'ai réussi

---
+++ grillemorpion.py
@@ -0,0 +1,13 @@
+grille = ["1","2","3","4","5","6","7","8","9"]
+
+def affichage_grille():
+    print("\n")
+    print("-------------")
+    print("|",grille[0],"|",grille[1],"|",grille[2],"|")
+    print("-------------")
+    print("|",grille[3],"|",grille[4],"|",grille[5],"|")
+    print("-------------")
+    print("|",grille[6],"|",grille[7],"|",grille[8],"|")
+    print("\n")
+
+affichage_grille()
